a7f025abf94fae1a9a32d1009d2e2e62493bba19
Haydn Ewers <haydn@particlesystem.com>
2013-02-11 21:52:39 +1030

    Improve the layouts. Cleanup the generator file.

--- lib/generators/ornament/ornament_generator.rb
+++ lib/generators/ornament/ornament_generator.rb
@@ -2,18 +2,39 @@ class OrnamentGenerator < Rails::Generators::Base
 
   source_root File.expand_path("../templates", __FILE__)
 
-  class_option :gems,      :type => :boolean, :default => true
-  class_option :core,      :type => :boolean, :default => true
-  class_option :prototype, :type => :boolean, :default => true
-  class_option :theme,     :type => :boolean, :default => true
-  class_option :extras,    :type => :boolean, :default => true
-  class_option :cleanup,   :type => :boolean, :default => true
+  class_option :gems,       :type => :boolean, :default => true
+  class_option :core,       :type => :boolean, :default => true
+  class_option :prototype,  :type => :boolean, :default => true
+  class_option :theme,      :type => :boolean, :default => true
+  class_option :ie_support, :type => :boolean, :default => true
+  class_option :layouts,    :type => :boolean, :default => true
+  class_option :cleanup,    :type => :boolean, :default => true
+
+  COMPONENT_IMAGES = [
+    "header/logo.png"
+  ]
+
+  COMPONENT_STYLESHEETS = [
+    "_align.css.scss",
+    "_button.css.scss",
+    "_clearfix.css.scss",
+    "_field.css.scss",
+    "_float.css.scss",
+    "_footer.css.scss",
+    "_header.css.scss",
+    "_heading.css.scss",
+    "_island.css.scss",
+    "_layout.css.scss",
+    "_rhythm.css.scss",
+    "_nav.css.scss",
+    "_split.css.scss",
+    "_table.css.scss"
+  ]
 
   def gems
     if options.gems?
       gem_group :assets do
         gem "sass-rails",    "~> 3.2.3"
-        gem "coffee-rails",  "~> 3.2.1"
         gem "uglifier",      ">= 1.0.3"
         gem "compass-rails", "~> 1.0.3"
       end
@@ -39,54 +60,42 @@ class OrnamentGenerator < Rails::Generators::Base
 
   def prototype
     if options.prototype?
-      copy_file "app/assets/images/prototype/header/logo.png"
-      copy_file "app/assets/stylesheets/prototype/_align.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_button.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_clearfix.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_field.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_float.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_footer.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_header.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_heading.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_island.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_layout.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_rhythm.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_nav.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_split.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_table.css.scss"
+      COMPONENT_IMAGES.each do |file|
+        copy_file "app/assets/images/prototype/#{file}"
+      end
+      COMPONENT_STYLESHEETS.each do |file|
+        copy_file "app/assets/stylesheets/prototype/#{file}"
+      end
     end
   end
 
   def theme
     if options.theme?
-      copy_file "app/assets/images/prototype/header/logo.png",          "app/assets/images/theme/header/logo.png"
-      copy_file "app/assets/stylesheets/prototype/_align.css.scss",     "app/assets/stylesheets/theme/_align.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_button.css.scss",    "app/assets/stylesheets/theme/_button.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_clearfix.css.scss",  "app/assets/stylesheets/theme/_clearfix.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_field.css.scss",     "app/assets/stylesheets/theme/_field.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_float.css.scss",     "app/assets/stylesheets/theme/_float.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_footer.css.scss",    "app/assets/stylesheets/theme/_footer.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_header.css.scss",    "app/assets/stylesheets/theme/_header.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_heading.css.scss",   "app/assets/stylesheets/theme/_heading.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_island.css.scss",    "app/assets/stylesheets/theme/_island.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_layout.css.scss",    "app/assets/stylesheets/theme/_layout.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_rhythm.css.scss",    "app/assets/stylesheets/theme/_rhythm.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_nav.css.scss",       "app/assets/stylesheets/theme/_nav.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_split.css.scss",     "app/assets/stylesheets/theme/_split.css.scss"
-      copy_file "app/assets/stylesheets/prototype/_table.css.scss",     "app/assets/stylesheets/theme/_table.css.scss"
+      COMPONENT_IMAGES.each do |file|
+        copy_file "app/assets/images/prototype/#{file}", "app/assets/images/theme/#{file}"
+      end
+      COMPONENT_STYLESHEETS.each do |file|
+        copy_file "app/assets/stylesheets/prototype/#{file}", "app/assets/stylesheets/theme/#{file}"
+      end
     end
   end
 
-  def extras
-    if options.extras?
-      copy_file "app/views/layouts/application.html.erb"
+  def ie_support
+    if options.ie_support?
       copy_file "vendor/assets/javascripts/IE9.js"
+    end
+  end
+
+  def layouts
+    if options.layouts?
+      template "app/views/layouts/global.html.erb"
+      copy_file "app/views/layouts/application.html.erb"
     end
   end
 
   def cleanup
     if options.cleanup?
-      if yes?("Remove the current 'application.css' file?")
+      if yes?("Remove the existing 'application.css' file?")
         remove_file "app/assets/stylesheets/application.css"
       end
     end
