f5f77be881bde858401fe6fd5e24bc4e18f99281
dbaines <dbaines@gmail.com>
2013-12-06 12:06:28 +1030

    Removed turbolinks

--- lib/generators/ornament/ornament_generator.rb
+++ lib/generators/ornament/ornament_generator.rb
@@ -33,7 +33,6 @@ class OrnamentGenerator < Rails::Generators::Base
           gem "sass-rails",     "~> 3.2.3"
           gem "uglifier",       ">= 1.0.3"
           gem "compass-rails",  "~> 1.0.3"
-          gem "turbolinks",     "~> 1.3.0"
         end
       end
 
