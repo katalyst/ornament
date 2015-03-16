8c96e4442474013792a51440bc76504e987609ea
David Baines <dbaines@gmail.com>
2015-03-16 20:58:10 +1030

    Started a show/hide code sample feature

--- test/dummy/app/helpers/application_helper.rb
+++ test/dummy/app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
       raw(@html_encoder.encode(code))
   end
 
+  def show_code(code)
+    toggle_id = Digest::SHA1.hexdigest([Time.now, rand].join)
+    code_view = ""
+    code_view = "<div>"
+    code_view += "  <div class='sg-toggle-code'><span data-toggle-anchor='#{toggle_id}' data-toggle-visible-label='Hide code sample'>View code sample</span></div>"
+    code_view += "  <div data-toggle='#{toggle_id}'><pre class='sg-pre prettyprint'>#{render_source(code)}</pre></div>".html_safe
+    code_view += "</div>"
+    code_view.html_safe
+  end
+
 end
