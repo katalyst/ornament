f28c138404b1e1b4302ac4f4ea54f90e407a3c7c
dbaines <dbaines@gmail.com>
2016-02-08 12:13:30 +1030

    Added a default secondary colour and updated the styleguide controller

--- lib/generators/ornament/templates/app/controllers/styleguide_controller.rb
+++ lib/generators/ornament/templates/app/controllers/styleguide_controller.rb
@@ -1,5 +1,7 @@
 class StyleguideController < ActionController::Base
-  layout "styleguide/appliction"
+  layout "styleguide/ornament"
+
+  include CommonControllerActions
 
   def lightbox_ajax_sample
     render layout: "lightbox_ajax"
