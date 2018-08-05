f79df722e8d4b09f0fd575614e872c5252742740
dbaines <dbaines@gmail.com>
2018-08-06 09:25:54 +0930

    Fixed homepage route for the styleguide

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,6 +1,5 @@
 Dummy::Application.routes.draw do
 
-  get '/styleguide' => 'styleguide#index', as: :styleguide
   get '/styleguide/:action' => 'styleguide'
   get '/test-404' => 'errors#404'
 
@@ -8,6 +7,6 @@ Dummy::Application.routes.draw do
   get '/site' => 'service_worker#webmanifest', format: :webmanifest, as: :webmanifest
   get '/service-worker' => 'service_worker#index', format: :js, as: :service_worker
 
-  root :to => 'styleguide#index'
+  root :to => 'styleguide#index', as: :styleguide
 
 end
